c32a7d1dd1ea3ff6a0467e431883e3b3384c064d
inman <sf990668852@gmail.com>
2020-08-23 23:40:47 +0800

    add some comment

--- examples/src/demos/MeshLine.js
+++ examples/src/demos/MeshLine.js
@@ -7,17 +7,18 @@ extend(meshline)
 
 //單一MeshLine生產工廠
 function Fatline({ curve, width, color, speed }) {
-  const material = useRef()
-  useFrame(() => (material.current.uniforms.dashOffset.value -= speed))
+  const lineRef = useRef(null)
+  useFrame(() => (lineRef.current.uniforms.dashOffset.value -= speed))
+  //console.log('lineRef: ', lineRef)
   return (
     <mesh>
       <meshLine attach="geometry" vertices={curve} />
       <meshLineMaterial
         attach="material"
-        ref={material}
-        transparent
-        depthTest={false}
+        ref={lineRef}
+        depthTest={false} //調成true看看
         lineWidth={width}
+        transparent
         color={color}
         dashArray={0.1}
         dashRatio={0.9}
@@ -31,14 +32,21 @@ function Lines({ count, colors }) {
   const lines = useMemo(
     () =>
       new Array(count).fill().map(() => {
+        //產生隨機position ([-10,10], [-10,10], [-10,10])
         const pos = new THREE.Vector3(10 - Math.random() * 20, 10 - Math.random() * 20, 10 - Math.random() * 20)
-        //調成 new Array(3)或 new Array(300)看看
+
+        //調成 new Array(3)或 new Array(300)看看 30表示每條mech自身的點數量
         const points = new Array(30)
           .fill()
           .map(() =>
             pos.add(new THREE.Vector3(4 - Math.random() * 8, 4 - Math.random() * 8, 2 - Math.random() * 4)).clone()
           )
+        //console.log(points)
+
+        //每條mech用數值方法取出1000個點代表
         const curve = new THREE.CatmullRomCurve3(points).getPoints(1000)
+        //console.log(curve)
+
         return {
           color: colors[parseInt(colors.length * Math.random())],
           width: Math.max(0.1, 0.65 * Math.random()),
@@ -48,7 +56,7 @@ function Lines({ count, colors }) {
       }),
     [colors, count]
   )
-  console.log(lines)
+  //console.log(lines)
   return lines.map((props, index) => <Fatline key={index} {...props} />)
 }
 
@@ -57,6 +65,8 @@ function Rig({ mouse }) {
   useFrame(() => {
     camera.position.x += (mouse.current[0] / 50 - camera.position.x) * 0.05
     camera.position.y += (-mouse.current[1] / 50 - camera.position.y) * 0.05
+
+    //console.log(mouse.current[0], -mouse.current[1])
 
     //調成這樣看看
     //camera.position.x += (mouse.current[0] / 5 - camera.position.x) * 0.05
